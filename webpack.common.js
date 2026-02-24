f65b6e858282294f9d1cb72e3c9941853a587f11
ahnafabid <aabid6000@gmail.com>
2026-02-24 14:54:50 +0900

    setup configuration of webpack and initial module

---
+++ webpack.common.js
@@ -0,0 +1,36 @@
+import path from 'node:path';
+import htmlWebpackPlugin from 'html-webpack-plugin';
+import test from 'node:test';
+
+export default {
+    entry: {
+        app: './src/index.js',
+    },
+    plugins: [
+        new htmlWebpackPlugin ({
+            title: 'Production',
+            template: './src/template.html',
+        }),
+    ],
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(import.meta.dirname, 'dist'),
+        clean: true,
+    },
+    module: {
+        rules: [
+            {
+            test: /\.css$/i,
+            use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(woff|woff2|ttf|otf)$/i,
+                type: 'asset/resource',
+            },
+        ],
+    },
+};
